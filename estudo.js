ae1b9074101f78022ce7d8145ca94944118fc98b
mah-sousabuenoo <111088679+mah-sousabuenoo@users.noreply.github.com>
2022-11-04 09:28:51 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,64 +1,70 @@
 
-deixe  tabuada  =  2 ;
-função  escreva ( ) {
-    documento . escreva  ( "Tabuada do "  +  tabuada  +  "<br>" )
-    documento . escreva ( tabuada  +  " x 1 = "  +  ( tabuada * 1 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 2 = "  +  ( tabuada * 2 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 3 = "  +  ( tabuada * 3 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 4 = "  +  ( tabuada * 4 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 5 = "  +  ( tabuada * 5 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 6 = "  +  ( tabuada * 6 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 7 = "  +  ( tabuada * 7 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 8 = "  +  ( tabuada * 8 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 9 = "  +  ( tabuada * 9 )  +  "<br>" ) ;
-    documento . escreva ( tabuada  +  " x 10 = "  +  ( tabuada * 10 )  +  "<br>" ) ;
+let tabuada = 8;
+
+function escreva(){
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
-let  lista  =  [ "Jesus" ,  "Spessato" ,  "Jorge" ,  "Duarte" ,  "Waldete" ,  "Raquel" ]
+let lista = ["Jorge","Duarte","Waldete","Raquel",];
 
-
-função  mostra ( ) {
-    documento . escreva ( lista . comprimento  +  "<br>" ) ;
-    for ( let  i  =  0 ;  i  <  lista . length ;  i ++ ) {
-        documento . escreva ( lista [ i ]  +  "<br>" ) ;
+function mostra(){
+    document.write(lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write(lista[i] + "<br>");
     }
 }
 
-função  mostraTabuada ( ) {
-    for ( seja  i  =  1 ;  i  <=  10 ;  i ++ ) {
-        documento . escreva ( "o valor do i"  +  i  +  "<br>" ) ;
+function mostraTabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("O valor do i " + i + "<br>");
     }
-}    
-função  total ( ) {
-    deixe  v  =  documento . getElementById ( "valor" ) . valor ;
-    seja  j  =  documento . getElementById ( "juros" ) . valor ;
-    seja  t  =  documento . getElementById ( "meses" ) . valor ;
-    seja  r  =  0 ;
-    for ( deixe  i  = 1 ;  i  <= t ;  i ++ ) {
-        r  =  v  *  ( 1  +  ( j / 100 ) ) ;
-        documento . escreva  ( "Mês "  +  i  +  " - valor: "  +  r  +  "<br>" ) ;
-        v  =  r ;
+}
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    if (!Number(v)){
+        alert("O campo valor deve ser números");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
     }
-    documento . escreva  ( "Resultado: "  +  r ) ;
+    if (!Number(j)){
+        alert("O campo juros deve ser números");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if (!Number(t)){
+        alert("O campo meses deve ser números");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+
+    let r = 0;
+    let texto = "";
+    for(let i =1; i <=t; i++){
+        r = v * (1 + (j/100));
+        //document.write("Mês " + i + " - valor: " + moeda(r) + "<br>");
+        texto += "Mês " + i + ": " + moeda(r) + "<br>";
+        v = r;
+    }
+    document.getElementById("mes").innerHTML = texto;
+    document.getElementById("total").innerHTML = moeda(r);
+    //document.write("Resultado: " + moeda(r));
 }
-
-function  somaNota ( ) {
-    seja  n1  =  documento . getElementById ( "b1" ) . valor ;
-    seja  n2  =  documento . getElementById ( "b2" ) . valor ;
-    seja  n3  =  documento . getElementById ( "b3" ) . valor ;
-    deixe  n4  =  documento . getElementById ( "b4" ) . valor ;
-
-    let  r  =  Número ( n1 ) + Número ( n2 ) + Número ( n3 ) + Número ( 4 ) ;
-
-    documento . getElementById ( "resul" ) . innerHTML  =  "Soma:"  +  r
-
-    função  somaMedia ( ) {
-        seja  n1  =  documento . getElementById ( "b1" ) . valor ;
-        seja  n2  =  documento . getElementById ( "b2" ) . valor ;
-        seja  n3  =  documento . getElementById ( "b3" ) . valor ;
-        deixe  n4  =  documento . getElementById ( "b4" ) . valor ;
-    
-        let  r  =  Número ( n1 ) + Número ( n2 ) + Número ( n3 ) + Número ( 4 ) ;
-    
-        documento . getElementById ( "media" ) . innerHTML  =  "Mídia:"  +  r
-}
+Footer
